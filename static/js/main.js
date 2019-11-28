2396127b4f8a246c6c9c606a2017acd94e9c49df
Dillon Temple <momo@Dillons-MacBook-Pro.local>
2019-11-27 21:00:02 -0800

    Final

--- static/js/main.js
+++ static/js/main.js
@@ -7,65 +7,86 @@ $(document).ready(function(){
     }
 
     $('.cast_button').hide();
+    $('.merch').hide();
+    $('.shirt').hide();
+    $('.about_cast').hide();
 
-    function randomvid(){
+    // function randomvid(){
 
-        var playlist = [
-            "https://www.youtube.com/watch?v=Y7Uis-2V8NY",
-            "https://www.youtube.com/watch?v=5_V2OSXICSE",
-            "https://www.youtube.com/watch?v=yS51ubG1Afc",
-            "https://www.youtube.com/watch?v=pT9ULBgw8xI",
-            "https://www.youtube.com/watch?v=AR9ECB__pXk",
-            "https://www.youtube.com/watch?v=lAwewggVFzg",
-            "https://www.youtube.com/watch?v=otySwF3SjS0",
-            "https://www.youtube.com/embed/g7xcU8Ow_Jk",
-            "https://www.youtube.com/embed/EuQjm4hu1So",
-            "https://www.youtube.com/embed/SbU9LDvEm7E",
-            "https://www.youtube.com/embed/6KM5BhcIt7Y",
-            "https://www.youtube.com/embed/tEJlrtBfnk8",
-            "https://www.youtube.com/embed/Jnv219F7wJc",
-            "https://www.youtube.com/embed/kZCC-p9jvCE",
-            "https://www.youtube.com/embed/eRAaGxFQNJE",
-            "https://www.youtube.com/embed/nwtpUhEHDHs",
-            "https://www.youtube.com/embed/ipVz0kcHH3c",
-            "https://www.youtube.com/embed/4qJCVknwY8s"
-        ]
+    //     var playlist = [
+    //         "https://www.youtube.com/watch?v=Y7Uis-2V8NY",
+    //         "https://www.youtube.com/watch?v=5_V2OSXICSE",
+    //         "https://www.youtube.com/watch?v=yS51ubG1Afc",
+    //         "https://www.youtube.com/watch?v=pT9ULBgw8xI",
+    //         "https://www.youtube.com/watch?v=AR9ECB__pXk",
+    //         "https://www.youtube.com/watch?v=lAwewggVFzg",
+    //         "https://www.youtube.com/watch?v=otySwF3SjS0",
+    //         "https://www.youtube.com/embed/g7xcU8Ow_Jk",
+    //         "https://www.youtube.com/embed/EuQjm4hu1So",
+    //         "https://www.youtube.com/embed/SbU9LDvEm7E",
+    //         "https://www.youtube.com/embed/6KM5BhcIt7Y",
+    //         "https://www.youtube.com/embed/tEJlrtBfnk8",
+    //         "https://www.youtube.com/embed/Jnv219F7wJc",
+    //         "https://www.youtube.com/embed/kZCC-p9jvCE",
+    //         "https://www.youtube.com/embed/eRAaGxFQNJE",
+    //         "https://www.youtube.com/embed/nwtpUhEHDHs",
+    //         "https://www.youtube.com/embed/ipVz0kcHH3c",
+    //         "https://www.youtube.com/embed/4qJCVknwY8s"
+    //     ]
 
-        var picked = {};
+    //     var picked = {};
 
-        for(i = 0; i < 3; i++){
+    //     for(i = 0; i < 3; i++){
 
             
-            var random = Math.floor(Math.random() * playlist.length);
+    //         var random = Math.floor(Math.random() * playlist.length);
 
-            if(picked[random] === 1){
-                i--
-                continue;
-            } 
-            else {
-                picked[random] = 1;
-            }
+    //         if(picked[random] === 1){
+    //             i--
+    //             continue;
+    //         } 
+    //         else {
+    //             picked[random] = 1;
+    //         }
 
-            var video = playlist[random];
-            document.getElementById("re" + i).src = video;
+    //         var video = playlist[random];
+    //         document.getElementById("re" + i).src = video;
 
-        }
-    }
+    //     }
+    // }
 
-    randomvid();
+    // randomvid();
 
     $(window).scroll(function (event) {
         var scroll = $(window).scrollTop();
         var bodyh = document.body.clientHeight;
+        var percentage = scroll / bodyh * 100;
         console.log("Scroll height:" + scroll);
         console.log("Page Height:" + bodyh);
-        console.log("Percentage:" + (scroll / bodyh * 100));
+        console.log("Percentage:" + percentage);
+
+        if(percentage > 42){
+            $('.merch').slideDown(1000);
+            $('.shirt').slideDown(1000);
+        } 
+        if(percentage < 42){
+            $('.merch').slideUp();
+            $('.shirt').slideUp();
+        }
+
+        if(percentage > 63.5){
+            $('.about_cast').fadeIn(1500);
+        }
+
+        if(percentage < 63.5){
+            $('.about_cast').fadeOut(750);
+        }
     });;
 
     var bill = "Billy doesn't remember the name of the town he was born in. He does remember loving pizza as long as he can remember though. He also likes when others enjoy pizza as well. If you see him, give him a hug.";
-    var jen = "Jennell generally takes photos, holds down the Pizza Sutra social media, answers emails and is basically the April O' Neil of this pizza crew";
-    var serg = "Sergio was born and raised in Milwaukee Wisconsin on a steady diet of PBR, cheese curds, and pizza. His belly was forged in the depths of the hottest bar deep friers of the Midwest. He likes to make things like movies and video and drawings"
-    var david = "I like Grog!"
+    var jen = "Jennell generally takes photos, holds down the Pizza Sutra social media, answers emails and is basically the April O' Neil of this pizza crew.";
+    var serg = "Sergio was born and raised in Milwaukee Wisconsin on a steady diet of PBR, cheese curds, and pizza. His belly was forged in the depths of the hottest bar deep friers of the Midwest. He likes to make things like movies and video and drawings."
+    var david = "David likes BMX, and BMX related accessories."
     const circleheight = document.getElementById("circlemaster").style.marginTop
     console.log(circleheight);
 
@@ -79,7 +100,7 @@ $(document).ready(function(){
             $(".circle").css("margin-top", "0em");
         }
         $(this).addClass('bubble');
-        $(".about_cast_header").html("BILLY ZASTROW");
+        $(".about_cast_header").html("BILLY");
         $(".circle2").hide();
         $(".circle3").hide();
         $(".circle4").hide();
@@ -94,7 +115,7 @@ $(document).ready(function(){
             $(".circle").css("margin-top", "0em");
         }
         $(this).addClass('bubble');
-        $(".about_cast_header").html("SERGIO ESPINO");
+        $(".about_cast_header").html("SERGIO");
         $(".circle1").hide();
         $(".circle3").hide();
         $(".circle4").hide();
@@ -109,7 +130,7 @@ $(document).ready(function(){
             $(".circle").css("margin-top", "0em");
         }
         $(this).addClass('bubble');
-        $(".about_cast_header").html("JENNELL JENNEY");
+        $(".about_cast_header").html("JENNELL");
         $(".circle2").hide();
         $(".circle1").hide();
         $(".circle4").hide();
@@ -124,7 +145,7 @@ $(document).ready(function(){
             $(".circle").css("margin-top", "0em");
         }
         $(this).addClass('bubble');
-        $(".about_cast_header").html("DAVID CORCAN");
+        $(".about_cast_header").html("DAVID");
         $(".circle2").hide();
         $(".circle3").hide();
         $(".circle1").hide();
